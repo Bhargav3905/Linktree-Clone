d043dae0ea18d81a775506bc915c418ea6c2ebd9
Bhargav3905 <prajapatibhargavk@gmail.com>
2026-02-20 18:57:41 +0530

    Updated .next

--- .next/static/chunks/app_layout_68b267f5.js
+++ .next/static/chunks/app_layout_68b267f5.js
@@ -2,7 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__501b62d3._.css",
-  "static/chunks/_a612dd36._.js"
+  "static/chunks/node_modules_next_b3198c5d._.js",
+  "static/chunks/components_cce63503._.js"
 ],
     source: "dynamic"
 });
