4abc65899d7488a825ad0ee882a12c8e223693cf
Bhargav3905 <prajapatibhargavk@gmail.com>
2026-02-23 22:56:13 +0530

    Updated Readme

--- .next/static/chunks/app_layout_68b267f5.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__501b62d3._.css",
-  "static/chunks/node_modules_next_b3198c5d._.js",
-  "static/chunks/components_cce63503._.js"
-],
-    source: "dynamic"
-});
